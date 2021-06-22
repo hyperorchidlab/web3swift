2f1ea007712e862ae5dc3eda9e261e5ddd2e6e6c
hopwesley <ws@sinoc.io>
2021-06-22 12:20:49 +0800

    for new version build

--- Sources/web3swift/Web3/Web3+WebsocketProvider.swift
+++ Sources/web3swift/Web3/Web3+WebsocketProvider.swift
@@ -92,9 +92,11 @@ public protocol Web3SocketDelegate {
 
 /// The default websocket provider.
 public class WebsocketProvider: Web3Provider, IWebsocketProvider, WebSocketDelegate {
-//        public func didReceive(event: WebSocketEvent, client: WebSocket) {
-//                //TODO::
-//        }
+
+        
+        public func didReceive(event: WebSocketEvent, client: WebSocket) {
+                //TODO::
+        }
         
     public func sendAsync(_ request: JSONRPCrequest, queue: DispatchQueue) -> Promise<JSONRPCresponse> {
         return Promise(error: Web3Error.inputError(desc: "Sending is unsupported for Websocket provider. Please, use \'sendMessage\'"))
